cbd34745679927c19835c58e1bbe3e84d57d3b73
sdm1126 <sdm1126@naver.com>
2021-12-03 19:37:53 +0900

    Git conflict 수정

--- php/adminpage_inquiry_read.php
+++ php/adminpage_inquiry_read.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
 <?php
 include_once('../db/db_connector.php');
 if (isset($_SESSION['session_id'])) {
@@ -17,48 +14,48 @@ if (isset($_SESSION['session_id'])) {
         $row = mysqli_fetch_assoc($result);
         if (mysqli_num_rows($result) > 0) {
 ?>
-            <!DOCTYPE html>
-            <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <link rel="stylesheet" href="../css/mypage_inquiry_read.css">
-                <link rel="stylesheet" href="../css/header.css">
-                <link rel="stylesheet" href="../css/footer.css">
-                <link rel="stylesheet" href="../css/aside.css">
-                <style>
-                    a:hover {
-                        color: black;
-                        text-decoration: none;
-                    }
-                </style>
-            </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/mypage_inquiry_read.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/aside.css">
+    <style>
+    a:hover {
+        color: black;
+        text-decoration: none;
+    }
+    </style>
+</head>
 
-            <body>
-                <div class="container">
-                    <?php
+<body>
+    <div class="container">
+        <?php
                     include('./header.php');
                     ?>
-                    <aside>
-                        <div>
-                            <ul>
-                                <li class="title">관리자
-                                    <br>페이지
-                                </li>
-                                <hr>
-                                <li><a href="./adminpage_user.php?page=1">전체 정보</a></li>
-                                <li><a href="./adminpage_reservation.php?page=1">전체 예약</a></li>
-                                <li><a href="./adminpage_inquiry_board.php?page=1"><b>전체 문의</a></b></li>
-                            </ul>
-                        </div>
-                    </aside>
-                    <main>
-                        <article class="h2">
-                            <h2>내 문의</h2>
-                            <?php
+        <aside>
+            <div>
+                <ul>
+                    <li class="title">관리자
+                        <br>페이지
+                    </li>
+                    <hr>
+                    <li><a href="./adminpage_user.php?page=1">전체 정보</a></li>
+                    <li><a href="./adminpage_reservation.php?page=1">전체 예약</a></li>
+                    <li><a href="./adminpage_inquiry_board.php?page=1"><b>전체 문의</a></b></li>
+                </ul>
+            </div>
+        </aside>
+        <main>
+            <article class="h2">
+                <h2>내 문의</h2>
+                <?php
                             $list_number = $_GET['no'];
                             $mb_id = $_GET['id'];
 
@@ -86,233 +83,143 @@ if (isset($_SESSION['session_id'])) {
                                 $list[$i] = $row;
                             }
                             ?>
-                        </article>
-                        <hr>
-                        <article class="form">
-                            <form action="./mypage_inquiry_update.php?no=<?= $list_number ?>" method="post">
-                                <table>
-                                    <tr>
-                                        <!-- 첫번째 줄 시작 -->
-                                        <td class="title">성명</td>
-                                        <td><?= $full_name ?></td>
-                                    </tr><!-- 첫번째 줄 끝 -->
-                                    <tr>
-                                        <!-- 첫번째 줄 시작 -->
-                                        <td class="title">아이디</td>
-                                        <td><?= $mb_id ?></td>
-                                    </tr><!-- 첫번째 줄 끝 -->
-                                    <tr>
-                                        <!-- 두번째 줄 시작 -->
-                                        <td class="title">제목</td>
-                                        <td><input name="title" type="text" id="title" value="<?= $title ?>" readonly></input></td>
-                                    </tr><!-- 두번째 줄 끝 -->
-                                    <tr>
-                                        <!-- 두번째 줄 시작 -->
-                                        <td class="title">내용</td>
-                                        <td><textarea name="content" id="content" cols="30" rows="10" readonly><?= $content ?></textarea></td>
-                                    </tr><!-- 두번째 줄 끝 -->
-                                    <tr>
-                                        <!-- 두번째 줄 시작 -->
-                                        <td class="title">휴대전화</td>
-                                        <td><?= $mobile ?></td>
-                                    </tr><!-- 두번째 줄 끝 -->
-                                    <tr>
-                                        <!-- 두번째 줄 시작 -->
-                                        <td class="title">이메일</td>
-                                        <td><?= $email ?></td>
-                                    </tr><!-- 두번째 줄 끝 -->
-                                </table>
-                        </article>
-                        <article class="button">
-                            <div>
-                                <input type="hidden" value="수 정" id="btn_modify" name="mode">
-                                <input type="hidden" value="삭 제" id="btn_delete" name="mode">
-                            </div>
-                            </form>
-                        </article>
-                        <?php
+            </article>
+            <hr>
+            <article class="form">
+                <form action="./mypage_inquiry_update.php?no=<?= $list_number ?>" method="post">
+                    <table>
+                        <tr>
+                            <!-- 첫번째 줄 시작 -->
+                            <td class="title">성명</td>
+                            <td><?= $full_name ?></td>
+                        </tr><!-- 첫번째 줄 끝 -->
+                        <tr>
+                            <!-- 첫번째 줄 시작 -->
+                            <td class="title">아이디</td>
+                            <td><?= $mb_id ?></td>
+                        </tr><!-- 첫번째 줄 끝 -->
+                        <tr>
+                            <!-- 두번째 줄 시작 -->
+                            <td class="title">제목</td>
+                            <td><input name="title" type="text" id="title" value="<?= $title ?>" readonly></input></td>
+                        </tr><!-- 두번째 줄 끝 -->
+                        <tr>
+                            <!-- 두번째 줄 시작 -->
+                            <td class="title">내용</td>
+                            <td><textarea name="content" id="content" cols="30" rows="10"
+                                    readonly><?= $content ?></textarea></td>
+                        </tr><!-- 두번째 줄 끝 -->
+                        <tr>
+                            <!-- 두번째 줄 시작 -->
+                            <td class="title">휴대전화</td>
+                            <td><?= $mobile ?></td>
+                        </tr><!-- 두번째 줄 끝 -->
+                        <tr>
+                            <!-- 두번째 줄 시작 -->
+                            <td class="title">이메일</td>
+                            <td><?= $email ?></td>
+                        </tr><!-- 두번째 줄 끝 -->
+                    </table>
+            </article>
+            <article class="button">
+                <div>
+                    <input type="hidden" value="수 정" id="btn_modify" name="mode">
+                    <input type="hidden" value="삭 제" id="btn_delete" name="mode">
+                </div>
+                </form>
+            </article>
+            <?php
                         for ($i = 0; $i < count($list); $i++) {
                         ?>
-                            <article class="textarea">
-                                <h3><?= $list[$i]['id'] ?></h3>
-                                <form action="./mypage_comment_update.php?id=<?= $mb_id ?>&no=<?= $list[$i]['no'] ?>" method="post">
-                                    <textarea name="content" class="comment_content" cols="30" rows="10" readonly><?= $list[$i]['content'] ?></textarea>
-                            </article>
-                            <article class="reply">
-                                <div>
-                                    <input type="button" class="comment_update" name="mode" value="수 정">
-                                    <input type="button" class="comment_delete" name="mode" value="삭 제">
-                                </div>
-                                </form>
-                            </article>
-                        <?php }
+            <article class="textarea">
+                <h3><?= $list[$i]['id'] ?></h3>
+                <form action="./mypage_comment_update.php?id=<?= $mb_id ?>&no=<?= $list[$i]['no'] ?>" method="post">
+                    <textarea name="content" class="comment_content" cols="30" rows="10"
+                        readonly><?= $list[$i]['content'] ?></textarea>
+            </article>
+            <article class="reply">
+                <div>
+                    <input type="button" class="comment_update" name="mode" value="수 정">
+                    <input type="button" class="comment_delete" name="mode" value="삭 제">
+                </div>
+                </form>
+            </article>
+            <?php }
                         mysqli_close($con); // 데이터베이스 접속 종료
                         ?>
-                        <article class="textarea">
-                            <h3>답 변</h3>
-                            <form action="./mypage_comment_update.php?id=<?= $mb_id ?>&no=<?= $list_number ?>" method="post">
-                                <textarea name="content" id="" cols="30" rows="10"></textarea>
-                        </article>
-                        <article class="reply">
-                            <div>
-                                <input type="submit" id="comment_insert" name="mode" value="등 록">
-                            </div>
-                            </form>
-                        </article>
-                    </main>
-                    <?php
+            <article class="textarea">
+                <h3>답 변</h3>
+                <form action="./mypage_comment_update.php?id=<?= $mb_id ?>&no=<?= $list_number ?>" method="post">
+                    <textarea name="content" id="" cols="30" rows="10"></textarea>
+            </article>
+            <article class="reply">
+                <div>
+                    <input type="submit" id="comment_insert" name="mode" value="등 록">
+                </div>
+                </form>
+            </article>
+        </main>
+        <?php
                     include('./footer.php');
                     ?>
-                </div>
-                <script>
-                    let btn_modify = document.querySelector('#btn_modify')
-                    let btn_delete = document.querySelector('#btn_delete')
-                    let title = document.querySelector('#title')
-                    let content = document.querySelector('#content')
+    </div>
+    <script>
+    let btn_modify = document.querySelector('#btn_modify')
+    let btn_delete = document.querySelector('#btn_delete')
+    let title = document.querySelector('#title')
+    let content = document.querySelector('#content')
 
-                    let comment_insert = document.querySelector('#comment_insert')
-                    let comment_update = document.querySelectorAll('.comment_update')
-                    let comment_delete = document.querySelectorAll('.comment_delete')
-                    let comment_content = document.querySelectorAll('.comment_content')
+    let comment_insert = document.querySelector('#comment_insert')
+    let comment_update = document.querySelectorAll('.comment_update')
+    let comment_delete = document.querySelectorAll('.comment_delete')
+    let comment_content = document.querySelectorAll('.comment_content')
 
-                    function delete_button_change(button) {
-                        if (button.value === "삭 제") {
-                            if (confirm('정말 삭제하시겠습니까?')) {
-                                button.type = "submit";
-                            }
-                        } else if (button.value === "취 소") {
-                            location.reload()
-                        }
-                    }
+    function delete_button_change(button) {
+        if (button.value === "삭 제") {
+            if (confirm('정말 삭제하시겠습니까?')) {
+                button.type = "submit";
+            }
+        } else if (button.value === "취 소") {
+            location.reload()
+        }
+    }
 
-                    btn_modify.addEventListener('click', () => {
-                        if (btn_modify.value === "수 정") {
-                            btn_modify.value = "확 인"
-                            btn_delete.value = "취 소"
-                            title.readOnly = false
-                            content.readOnly = false
-                        } else if (btn_modify.value === "확 인") {
-                            btn_modify.type = "submit"
-                        }
-                    })
+    btn_modify.addEventListener('click', () => {
+        if (btn_modify.value === "수 정") {
+            btn_modify.value = "확 인"
+            btn_delete.value = "취 소"
+            title.readOnly = false
+            content.readOnly = false
+        } else if (btn_modify.value === "확 인") {
+            btn_modify.type = "submit"
+        }
+    })
 
-                    btn_delete.addEventListener('click', () => {
-                        delete_button_change(btn_delete)
-                    })
+    btn_delete.addEventListener('click', () => {
+        delete_button_change(btn_delete)
+    })
 
-                    for (let i = 0; i < comment_update.length; i++) {
-                        comment_update[i].addEventListener('click', () => {
-                            if (comment_update[i].value === "수 정") {
-                                comment_update[i].value = "확 인"
-                                comment_delete[i].value = "취 소"
-                                comment_content[i].readOnly = false
-                            } else if (comment_update[i].value === "확 인") {
-                                comment_update[i].type = "submit"
-                            }
-                        })
+    for (let i = 0; i < comment_update.length; i++) {
+        comment_update[i].addEventListener('click', () => {
+            if (comment_update[i].value === "수 정") {
+                comment_update[i].value = "확 인"
+                comment_delete[i].value = "취 소"
+                comment_content[i].readOnly = false
+            } else if (comment_update[i].value === "확 인") {
+                comment_update[i].type = "submit"
+            }
+        })
 
-                        comment_delete[i].addEventListener('click', function() {
-                            delete_button_change(comment_delete[i])
-                        })
-                    }
-                </script>
-            </body>
+        comment_delete[i].addEventListener('click', function() {
+            delete_button_change(comment_delete[i])
+        })
+    }
+    </script>
+</body>
 
-            </html>
+</html>
 <?php
         }
     }
 }
 ?>
-=======
->>>>>>> a2f63a00d8c8b9caf7b8ba5decfba2769d5efae8
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/service_inquiry.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/footer.css">
-    <link rel="stylesheet" href="../css/aside.css">
-</head>
-
-<body>
-    <div class="container">
-        <!-- header -->
-        <?php
-            include('./header.php');
-        ?>
-
-        <!-- aside -->
-        <aside>
-            <div>
-                <ul>
-                    <li class="title">관리자
-                        <br>페이지
-                    </li>
-                    <hr>
-                    <li>전체 정보</li>
-                    <li>전체 예약</li>
-                    <li><b>전체 문의</b></li>
-                </ul>
-            </div>
-        </aside>
-
-        <!-- article -->
-        <main>
-            <article class="head">
-                <h2 id="h2">문의하기</h2>
-            </article>
-            <hr>
-            <article class="main">
-                <form action="">
-                    <table>
-                        <tr>
-                            <td class="title">성명</td>
-                            <td>(개인 정보에서 넘어오게 함)</td>
-                        </tr>
-                        <tr>
-<<<<<<< HEAD
-                            <td class="title">예약번호</td>
-                            <td><span></span></td>
-                        </tr>
-                        <tr>
-=======
->>>>>>> a2f63a00d8c8b9caf7b8ba5decfba2769d5efae8
-                            <td class="title">제목</td>
-                            <td><span></span></td>
-                        </tr>
-                        <tr>
-                            <td class="title" id="content">내용</td>
-                            <td><textarea name="" id="" cols="30" rows="10" disabled></textarea></td>
-                        </tr>
-                        <tr>
-                            <td class="title">휴대전화</td>
-                            <td>(개인 정보에서 넘어오게 함)</td>
-                        </tr>
-                        <tr>
-                            <td class="title">이메일</td>
-                            <td>(개인 정보에서 넘어오게 함)</td>
-                        </tr>
-                    </table>
-                </form>
-            </article>
-        </main>
-
-        <!-- footer -->
-        <?php
-            include('./footer.php');
-        ?>
-    </div>
-</body>
-
-<<<<<<< HEAD
-</html>
-=======
-</html>
->>>>>>> Stashed changes
->>>>>>> a2f63a00d8c8b9caf7b8ba5decfba2769d5efae8

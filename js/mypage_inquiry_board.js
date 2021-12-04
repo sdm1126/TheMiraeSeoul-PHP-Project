0a32d7ae970e967c2ff3268cf6c507b3ba6274dc
lji8804 <lji8804@gmail.com>
2021-12-04 19:58:24 +0900

    mypage_inquiry update

---
+++ js/mypage_inquiry_board.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    let select = document.querySelector('#select')
+    let search_str = document.querySelector('#search_str')
+    let error = document.querySelector('#error')
+    let submit = document.querySelector('#submit')
+    
+    
+    
+    search_str.addEventListener('keyup', function() {
+      let str = this.value
+      if (select.options[select.selectedIndex].value === 'written_date') {
+        var xhttp
+        if (str == "") {
+          error.innerHTML = ""
+          return
+        }
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState === this.DONE && this.status === 200) {
+            error.innerHTML = this.responseText
+          }
+        }
+        xhttp.open("GET", "search_str_check.php?q=" + str, true)
+        xhttp.send()
+    
+      }
+    })
+    
+    submit.addEventListener('click', function(){
+      if (select.options[select.selectedIndex].value === 'written_date'){
+          if(error.innerHTML){
+            search_str.value = ''
+          }
+      }
+    })
+    })
--- php/mypage_inquiry_board.php
+++ php/mypage_inquiry_board.php
@@ -28,13 +28,14 @@ if (isset($_SESSION['session_id'])) {
   <link rel="stylesheet" href="../css/footer.css">
   <link rel="stylesheet" href="../css/page.css">
   <link rel="stylesheet" href="../css/aside.css">
+  <script src="../js/mypage_inquiry_board.js"></script>
 </head>
 
 <body>
   <div class="container">
     <?php
-                    include('./header.php');
-                    ?>
+    include('./header.php');
+    ?>
     <aside>
       <div>
         <ul>
@@ -99,42 +100,8 @@ if (isset($_SESSION['session_id'])) {
   </div>
 </body>
 <script>
-let select = document.querySelector('#select')
-let search_str = document.querySelector('#search_str')
-let error = document.querySelector('#error')
-let submit = document.querySelector('#submit')
-
-document.querySelector('option[value="<?= $option ?>"]').selected = "selected"
-
-search_str.addEventListener('keyup', function() {
-  let str = this.value
-  if (select.options[select.selectedIndex].value === 'written_date') {
-    var xhttp
-    if (str == "") {
-      error.innerHTML = ""
-      return
-    }
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState === this.DONE && this.status === 200) {
-        error.innerHTML = this.responseText
-      }
-    }
-    xhttp.open("GET", "search_str_check.php?q=" + str, true)
-    xhttp.send()
-
-  }
-})
-
-submit.addEventListener('click', function(){
-  if (select.options[select.selectedIndex].value === 'written_date'){
-      if(error.innerHTML){
-        search_str.value = ''
-      }
-  }
-})
+  document.querySelector('option[value="<?= $option ?>"]').selected = "selected"
 </script>
-
 </html>
 <?php
         }
